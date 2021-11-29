d4a971798c96b1b0bee92ef1c79dd64e4672542d
fjavieral <jalvarez@ingesit.com>
2021-11-29 04:13:49 -0800

    OT 20204956

--- openeducat_surnames/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2021 Ingeos
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import models
--- openeducat_surnames/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2021 Ingeos
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import student
-from . import faculty
