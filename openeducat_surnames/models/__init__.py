d0cd38f569f0d472f019e7a9ecb1575179cc7f15
fjavieral <jalvarez@ingesit.com>
2021-11-29 01:33:50 -0800

    OT 20204956

---
+++ openeducat_surnames/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021 Ingeos
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import student
+from . import faculty
---
+++ openeducat_surnames/models/faculty.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Ingeos
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models, fields, api
+
+
+class OpFaculty(models.Model):
+    _inherit = 'op.faculty'
+
+    middle_name = fields.Char("Middle Name", size=128, required=True)
+    last_name = fields.Char("Last Name", size=128)
+
+    @api.onchange('first_name', 'middle_name', 'last_name')
+    def _onchange_name(self):
+        if not self.last_name:
+            self.name = str(self.first_name) + " " + str(self.middle_name)
+        else:
+            self.name = str(self.first_name) + " " + str(
+                self.middle_name) + " " + str(self.last_name)
